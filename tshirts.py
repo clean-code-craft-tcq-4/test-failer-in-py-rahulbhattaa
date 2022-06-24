2d1722686749ef797ca65819cb92f38d00799e1f
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-06-24 10:34:50 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -7,8 +7,10 @@ def size(cms):
     else:
         return 'L'
 
+def size_define():
+    assert(size(37) == 'S')
+    assert(size(40) == 'M')
+    assert(size(43) == 'L')
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
+size_define()
 print("All is well (maybe!)\n")
