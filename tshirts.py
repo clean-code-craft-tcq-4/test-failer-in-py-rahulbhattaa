f787f61b3e4939686f0c56fed10b6edc048d3cc4
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-06-28 10:39:24 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -2,16 +2,17 @@
 def size(cms):
     if cms < 38:
         return 'S'
-    elif cms > 38 and cms < 42:
+    elif cms >= 38 and cms < 42:
         return 'M'
     else:
         return 'L'
 
-def size_define():
+def test_tshirt_size():
     assert(size(37) == 'S')
     assert(size(40) == 'M')
     assert(size(43) == 'L')
     assert(size(38) == 'M')
 
-size_define()
-print("All is well (maybe!)\n")
+
+test_tshirt_size()
+print("All is well\n")
