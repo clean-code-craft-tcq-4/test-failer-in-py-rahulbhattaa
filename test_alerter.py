1031f2dff6aea2d83a142f820c35f9b10cfc5112
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-06-28 10:45:13 +0530

    Create test_alerter.py

---
+++ test_alerter.py
@@ -0,0 +1,15 @@
+import alerter
+import stub_alerter
+
+def test_alerter():
+  alerter.alert_in_celcius(400.5,stub_alerter.network_not_ok)
+  assert(alerter.alert_failure_count == 1)
+  alerter.alert_in_celcius(303.6,stub_alerter.network_not_ok)
+  assert(alerter.alert_failure_count == 2)
+  print(f'{alerter.alert_failure_count} alerts failed.')
+  alerter.alert_in_celcius(100,stub_alerter.network_ok)
+  assert(alerter.alert_failure_count == 2)
+  
+if __name__ == '__main__':
+  test_alerter()
+  print('All is well')
