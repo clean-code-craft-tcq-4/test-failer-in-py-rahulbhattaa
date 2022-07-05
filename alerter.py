137b263855395296e32f81bbb497ecbd99d0935b
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-07-05 14:57:20 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -1,14 +1,20 @@
-import stub_alerter
-
 alert_failure_count = 0
+threshold_value=180
 
+def network_alert_stub(celcius):
+    print(f'ALERT: Temperature is {celcius} celcius')
+    if(celcius <= float(threshold_value)):
+        return 200   # Return 200 for ok
+    else:
+        return 500   #Return 500 for not ok
+    
 def convert_farenheit_to_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
     return celcius
-     
-def alert_in_celcius(farenheit,network_status):
+
+def alert_in_celcius(farenheit):
     celcius = convert_farenheit_to_celcius(farenheit)
-    returnCode = stub_alerter.network_alert_stub(celcius,network_status)
+    returnCode = network_alert_stub(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
@@ -16,3 +22,10 @@ def alert_in_celcius(farenheit,network_status):
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
         alert_failure_count += 1
+
+
+alert_in_celcius(400.5)
+alert_in_celcius(303.6)
+print(f'{alert_failure_count} alerts failed.')
+assert(alert_failure_count==1)
+print('All is well (maybe!)')
