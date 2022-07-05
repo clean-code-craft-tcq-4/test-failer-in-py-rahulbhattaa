9f163b412db0caaef26abf382d26eefa9eef9c2c
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-07-05 15:11:32 +0530

    Update test_misaligned.py

--- test_misaligned.py
+++ test_misaligned.py
@@ -9,7 +9,7 @@ def test_pair_no(major_color,minor_color,expected_pair_no):
     
 def test_colors_and_pair_no(major_colors,minor_colors,expected_pair_sequence):
     resultant_color_code_manual=misaligned.print_color_map(major_colors,minor_colors)
-    assert (resultant_color_code_manual[20]==expected_pair_sequence)
+    assert (resultant_color_code_manual[25]==expected_pair_sequence)
 
 if __name__ == '__main__':
     test_pair_no("White","Blue",1)
