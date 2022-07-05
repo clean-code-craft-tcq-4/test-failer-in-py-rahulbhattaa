f38d05066f002733b01d6cc08f6baca0643ca06c
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-07-05 15:19:20 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,20 +1,21 @@
 
-major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-color_code_manual=[]
+def print_color_map():
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    color_map = []
+    
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            print(f'{i * 5 + j} | {major} | {minor}')
+            color_map.append(f'{i * 5 + j} | {major} | {minor}')
+                        
+    return len(major_colors) * len(minor_colors), color_map
 
-def get_pair_no_from_colors(major_color,minor_color):
-    major_index = major_colors.index(major_color)
-    minor_index = minor_colors.index(minor_color)
-    return major_index * len(minor_colors) + minor_index + 1
+result, color_map = print_color_map()
 
+assert (color_map[0] == '1 | White | Blue')
+assert (color_map[1] == '3 | White | Orange')
+assert (color_map[14] == '15 | Black | Slate')
+assert(result == 25)
 
-def print_color_map(major_colors,minor_colors):   
-    for i, major_col in enumerate(major_colors):
-        for j, minor_col in enumerate(minor_colors):
-         color_code_manual.append([i * 5 + j + 1 ,major_col,minor_col])   
-    return color_code_manual
-
-
-def format_color_code_manual(pair_number,major_color,minor_color):
-    return f'{pair_number}|{major_color}|{minor_color}'
+print("All is well (maybe!)\n")
